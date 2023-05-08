24039a75f792a92c9ee5ba7604cbfceadaaab93a
Alex Miranda <alex_miranda@outlook.com.br>
2023-05-08 20:52:55 -0300

    Update Audiencia_DoWhile.java
    Finalizado com ajuda do chat GPT

--- src/audiencia_DoWhile/Audiencia_DoWhile.java
+++ src/audiencia_DoWhile/Audiencia_DoWhile.java
@@ -7,40 +7,59 @@ public class Audiencia_DoWhile {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		int canal, canal_4 = 0, canal_5 = 0, canal_7 = 0, canal_12 = 0, totalCont = 0;
+		int canal,  pessoas, totalPessoas = 0;
+		
+		double percCanal_4 = 0, percCanal_5 = 0, percCanal_7 = 0, percCanal_12 = 0;
 		
 		do {
-			System.out.println("Informe o número do canal.");
+			System.out.println("Digite o número do canal (4, 5, 7 ou 12) ou 0 para sair: ");
 			canal = input.nextInt();
 			
-			if (canal ==4) {
-				canal_4 = canal_4 + 1;
-				totalCont = totalCont +1;
-			}
+			if (canal == 0) {
+	           break;
+	        }
 			
-			if (canal ==5) {
-				canal_5 = canal_5 + 1;
-				totalCont = totalCont +1;
-			}
+			System.out.println("Informe quantas pessoas está assistindo.");
+			pessoas = input.nextInt();
 			
-			if (canal ==7) {
-				canal_7 = canal_7 + 1;
-				totalCont = totalCont +1;
-			}
+			if (canal == 4) {
+				percCanal_4 += pessoas;
+            } else if (canal == 5) {
+            	percCanal_5 += pessoas;
+            } else if (canal == 7) {
+            	percCanal_7 += pessoas;
+            } else if (canal == 12) {
+            	percCanal_12 += pessoas;
+            }
 			
-			if (canal ==12) {
-				canal_12 = canal_12 + 1;
-				totalCont = totalCont +1;
-			}
+			totalPessoas += pessoas;
+
 			
 		} while (canal != 0);
 		
-		System.out.println();
-		System.out.println("O total de pessoas assistindo televisão é, " + totalCont);
-		System.out.println("A quantidade de pessoas assistindo o canal 4 é, " + canal_4);
-		System.out.println("A quantidade de pessoas assistindo o canal 5 é, " + canal_5);
-		System.out.println("A quantidade de pessoas assistindo o canal 7 é, " + canal_7);
-		System.out.println("A quantidade de pessoas assistindo o canal 12 é, " + canal_12);
-	}
+						
+						
+		System.out.println("\nRelatório de Audiência");
+        System.out.println("----------------------");
+
+        if (totalPessoas == 0) {
+            System.out.println("Não foi registrada nenhuma audiência.");
+        } else {
+        	percCanal_4 = (percCanal_4 / totalPessoas) * 100;
+        	percCanal_5 = (percCanal_5 / totalPessoas) * 100;
+        	percCanal_7 = (percCanal_7 / totalPessoas) * 100;
+			percCanal_12 = (percCanal_12 / totalPessoas) * 100;
+
+			System.out.println("\nTotal de Audiência = " + totalPessoas  );
+	        System.out.println("\n" );
+            System.out.printf("Canal 4: %.2f%%\n", percCanal_4);
+            System.out.printf("Canal 5: %.2f%%\n", percCanal_5);
+            System.out.printf("Canal 7: %.2f%%\n", percCanal_7);
+            System.out.printf("Canal 12: %.2f%%\n",percCanal_12);
+        }
+
+        input.close();
+    }
+	
 
 }
